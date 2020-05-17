ce1e027dcb60e0f2ff9896dc277fe4a2d77ae14d
Szym <szymmm@gmail.com>
2020-05-17 22:17:28 +0200

    Slider in boxView

--- src/views/BoxView/BoxView.js
+++ src/views/BoxView/BoxView.js
@@ -1,13 +1,47 @@
+import 'rc-slider/assets/index.css';
+import styles from "./Box.module.scss";
 import React from 'react';
-import AppContext from '../../context';
-import List from '../../components/List/List';
+import Slider from 'rc-slider';
 
-const ArticlesView = () => ( 
-  <AppContext.Consumer>
-    {(context) => (
-         <List items={context.article}/>
-    )}
-  </AppContext.Consumer>
+const wrapperStyle = { 
+ // width: 400, 
+  margin: 50 };
+
+
+const BoxView = () => (
+    <div style={wrapperStyle}>
+       <h1 className={styles.headline}>Ustawienia skrzynki</h1>
+       <p className={styles.description_main}>Ustaw swoje preferencje warzyw i owoców. Przesuń suwak by wybrać warzywa i owoce, które lubisz najbardziej, a także te których mógłbyś dostać mniej.</p>
+    
+      <p className={styles.description_slider}>Sałata masłowa</p>
+      <Slider
+        dotStyle={{borderColor: '#e9e9e9'}} activeDotStyle={{borderColor: '#e9e9e9'}}
+        trackStyle={{ backgroundColor: 'green' }}
+        handleStyle={{ borderColor: 'lightgreen' }}
+        min={1} max={3} startPoint={2} defaultValue={2} marks={{ 1: 'mniej', 2: 'średnio', 3: 'więcej' }} step={null} />
+    
+    <p className={styles.description_slider}>Sałata lodowa</p>
+    <Slider
+      dotStyle={{borderColor: '#e9e9e9'}} activeDotStyle={{borderColor: '#e9e9e9'}}
+      trackStyle={{ backgroundColor: 'green' }}
+      handleStyle={{ borderColor: 'lightgreen' }}
+      min={1} max={3} startPoint={2} defaultValue={2} marks={{ 1: 'mniej', 2: 'średnio', 3: 'więcej' }} step={null} />
+ 
+  <p className={styles.description_slider}>Sałata rzymska</p>
+  <Slider 
+    dotStyle={{borderColor: '#e9e9e9'}} activeDotStyle={{borderColor: '#e9e9e9'}}
+    trackStyle={{ backgroundColor: 'green' }}
+    handleStyle={{ borderColor: 'lightgreen' }}
+    min={1} max={3} startPoint={2} defaultValue={2} marks={{ 1: 'mniej', 2: 'średnio', 3: 'więcej' }} step={null} />
+
+ <p className={styles.description_slider}>Rukola</p>
+ <Slider
+  dotStyle={{borderColor: '#e9e9e9'}} activeDotStyle={{borderColor: '#e9e9e9'}}
+  trackStyle={{ backgroundColor: 'green' }}
+  handleStyle={{ borderColor: 'lightgreen' }}
+  min={1} max={3} startPoint={2} defaultValue={2} marks={{ 1: 'mniej', 2: 'średnio', 3: 'więcej' }} step={null} />
+</div>
 );
 
-export default ArticlesView;
+
+export default BoxView;
